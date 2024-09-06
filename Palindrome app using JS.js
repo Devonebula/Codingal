8e77f721b5f2abbd428c1a9826af763750e4470e
Devonebula <raghavkeshavagarwals@gmail.com>
2024-09-06 17:52:58 +0530

    4

---
+++ Palindrome app using JS.js
@@ -0,0 +1,20 @@
+// var s=prompt('Enter the string')
+// s1=s.split('').reverse().join('')
+// if(s==s1){
+//     alert('palindrome')
+// }
+// else{
+//     alert('not a palindrome')
+// }
+
+var s=prompt('Enter the string')
+s1=s.length
+for(i=0;i<s1/2;i++){
+    if(s[i]!==s[s1-1-i]){
+        break
+    }
+    else{
+        continue
+    }
+         
+    }
