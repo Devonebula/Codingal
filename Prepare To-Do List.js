01f445be31893c2ffce2ab380f6961c650df34d9
Devonebula <raghavkeshavagarwals@gmail.com>
2024-09-11 16:40:20 +0530

    7

---
+++ Prepare To-Do List.js
@@ -0,0 +1,14 @@
+function addtodo(){
+    var int=document.getElementById('input')
+    var ulist=document.getElementById('list')
+    var in_t=document.getElementById('input').value
+    var li=document.createElement('li')
+    li.textContent=in_t
+    var button=document.createElement('button')
+    button.textContent='Delete'
+    li.appendChild(button)
+    ulist.appendChild(li)
+    button.addEventListener('click',()=>{ulist.removeChild(li)})
+    int.value=''
+
+}
