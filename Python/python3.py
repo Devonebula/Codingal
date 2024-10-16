718a865b3844342f2cf132d8688d82d81c8fc5f6
Devonebula <raghavkeshavagarwals@gmail.com>
2024-10-16 16:42:23 +0530

    29

---
+++ Python/python3.py
@@ -0,0 +1,8 @@
+# a=int(input("Enter a Length:\t"))
+# b=int(input("Enter a Breadth:\t"))
+# area= a*b
+# print("Area of Rectangle:" ,area)
+
+a,b,c,d=input("Enter 4 Numbers: \t").split()
+sum=int(a)+int(b)+int(c)+int(d)
+print(sum)
