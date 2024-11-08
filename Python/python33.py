d718a36a01ed8af8aa755c05b67ce84edaa7a1ab
Devonebula <raghavkeshavagarwals@gmail.com>
2024-11-08 16:31:53 +0530

    43

---
+++ Python/python33.py
@@ -0,0 +1,6 @@
+i=int(input("Enter a number:\t"))
+a=1
+while a<=10:
+    
+    print(f"{i} * {a} = {i*a}")
+    a=a+1
---
+++ Python/python34.py
@@ -0,0 +1,4 @@
+a=int(input("Enter a number:\t"))
+b=int(input("Enter the power of the number:\t"))
+c=a**b
+print(f"{a} raised to the power {b} is {c}")
