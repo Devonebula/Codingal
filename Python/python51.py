467b9ccd0cb12732405ff63eebfbd557ad384fd3
Devonebula <raghavkeshavagarwals@gmail.com>
2024-11-26 20:30:06 +0530

    52

---
+++ Python/python51.py
@@ -0,0 +1,13 @@
+import turtle
+t=turtle.Turtle()
+s=turtle.Screen()
+s.bgcolor("black")
+t.color("white")
+t.shape("turtle")
+t.pencolor("red")
+t.pensize(3)
+for i in range(4):
+    t.fd(100)
+    t.lt(90)
+turtle.done()
+
