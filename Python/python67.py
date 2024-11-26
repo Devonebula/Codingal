24d933af0d6f2987dca868b292154b4d41a88a68
Devonebula <raghavkeshavagarwals@gmail.com>
2024-11-26 20:26:12 +0530

    50

--- Python/python67.py
+++ Python/python67.py
@@ -1,3 +1,14 @@
-a=input("Enter a number:\t")
-for i in a:
-    print(i)q
+decimal_number = int(input("Enter a decimal number: "))
+
+binary = ""
+
+while decimal_number > 0:
+    remainder = decimal_number % 2
+    binary = str(remainder) + binary  
+    decimal_number //= 2
+
+
+if binary == "":
+    binary = "0"
+
+print(f"The binary representation is: {binary}")
