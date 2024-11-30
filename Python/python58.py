c327998ad1b29ef5e3cdb6889a4211f48fe39e12
Devonebula <raghavkeshavagarwals@gmail.com>
2024-11-30 12:04:49 +0530

    56

---
+++ Python/python58.py
@@ -0,0 +1,10 @@
+for i in range(0, 3):
+    for j in range(0, i+1):
+        print("*", end=" ")
+    print()
+    
+print()
+for i in range(3, 0, -1):
+    for j in range(0, i):
+        print("*", end=" ")
+    print()
