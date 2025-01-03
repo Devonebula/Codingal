5cdc221f3ead9d600edc338a61b06dee2aa3a2e2
Devonebula <raghavkeshavagarwals@gmail.com>
2025-01-03 16:31:52 +0530

    77

--- Python/python85.py
+++ Python/python85.py
@@ -2,3 +2,9 @@ tup1=(1,2,3,4,5,6)
 tup2=(6,5,4,3,2,1)
 for i in tup1:
     print(i*tup2[tup1.index(i)])
+    
+    
+    
+fruits= ("apple", "banana", "cherry", "kiwi", "mango")
+for i,v in enumerate(fruits):
+    print(i,v)
---
+++ Python/python98.py
@@ -0,0 +1,4 @@
+zip1=[10,20,30]
+zip2=[100,100,100]
+r=(map(lambda x,y:x+y,zip1,zip2))
+print(list(r))
