ec2a01fb6102e09869a9511e4a34af9d9c21d384
Devonebula <raghavkeshavagarwals@gmail.com>
2025-01-09 22:06:13 +0530

    82

---
+++ Python/python118.py
@@ -0,0 +1,7 @@
+max=int(input("Enter the maximum number:\t"))
+odd_num=[i for i in range(1,max+1) if i%2!=0]
+print(odd_num)
+
+fruits=["apple","banana","cherry","kiwi","mango"]
+list=[i.capitalize() for i in fruits]
+print(list)
