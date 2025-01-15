47ce8b3c93f3a898b8336db0b41781540f8aee5e
Devonebula <raghavkeshavagarwals@gmail.com>
2025-01-15 21:25:56 +0530

    88

--- Python/python120.py
+++ Python/python120.py
@@ -1,21 +1,21 @@
-class breed:
+class Breed:
     def __init__(self, name, age, breed):
         self.name = name
         self.age = age
         self.breed = breed
     def display(self):
         print(f"name:{self.name}\nage:{self.age}\nbreed:{self.breed}")
-class dog:
+class Dog:
     def __init__(self, name):
         self.name = name
     def display(self):
         print(f"name:{self.name}")
-    def showbreed(self):
+    def show_breed(self):
         breed=input("Enter your breed:\t")
         age=int(input("Enter your age:\t"))
-        dog=breed(self.name, age, breed)
+        dog=Breed(self.name, age, breed)
         dog.display()
-dog1=dog("kiku")
-dog2=dog("chiku")
-dog1.showbreed()
-dog2.showbreed()
+dog1=Dog("kiku")
+dog2=Dog("chiku")
+dog1.show_breed()
+dog2.show_breed()
