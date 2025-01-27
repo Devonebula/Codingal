f443c16e651aefb0094c7d4655bb0f7ee494bb4a
Devonebula <raghavkeshavagarwals@gmail.com>
2025-01-27 16:53:10 +0530

    100

---
+++ Python/python144.py
@@ -0,0 +1,24 @@
+import pygame
+pygame.init()
+
+screen = pygame.display.set_mode((400,400))
+screen.fill((0, 0 ,0))
+
+pygame.display.set_caption("My First game screen")
+
+red=(255, 0, 0)
+
+rect_width = 100
+rect_height = 80
+x = (400 - rect_width) // 2
+y = (400 - rect_height) // 2
+
+done=False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.draw.rect(screen, red, pygame.Rect(x, y, rect_width, rect_height))
+    pygame.draw.circle(screen, (255,255,255), (200,200), 90, 3)
+    pygame.
+    pygame.display.flip()
