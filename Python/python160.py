a368dbefa555dbbd0fd426897380f9224040c73f
Devonebula <raghavkeshavagarwals@gmail.com>
2025-02-12 16:36:54 +0530

    111

---
+++ Python/python160.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from datetime import date
+
+
+t=Tk()
+t.title('Age Calculator App')
+t.geometry('400x400')
+
+l=Label(text='Age Calculator App' , font=('Arial', 30), fg='green')
+l.pack()
+
+name=Label(text='Enter your Name')
+name_entry=Entry()
+
+l5=Label(text='Enter your Year')
+e5=Entry()
+
+l4=Label(text='Enter your Month')
+e4=Entry()
+
+l3=Label(text='Enter your Date')
+e3=Entry()
+
+
+
+
+
+# btn=Button(text='Calculate Age', command=calculate_age)
+
+def calculate_age():
+    name    
+
+
+t.mainloop()
